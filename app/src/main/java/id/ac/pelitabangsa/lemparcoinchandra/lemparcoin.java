f5c3a77af5a3f6e19fec5bb27c6c6f816e4f81fd
chens17 <csaputra456@gmail.com>
2021-04-02 10:43:22 +0700

    project lemparkoinchandra

---
+++ app/src/main/java/id/ac/pelitabangsa/lemparcoinchandra/lemparcoin.java
@@ -0,0 +1,58 @@
+package id.ac.pelitabangsa.lemparcoinchandra;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class lemparcoin extends Activity implements View.OnClickListener {
+    private Button buttonLempar;
+    private Button buttonUlangi;
+    private Button buttonKeluar;
+    private Random acak;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        acak= new Random();
+        setContentView(R.layout.lemparcoin);
+        buttonLempar=(Button)findViewById(R.id.buttonlempar);
+        buttonUlangi=(Button)findViewById(R.id.buttonulangi);
+        buttonKeluar=(Button)findViewById(R.id.buttonkeluar);
+        buttonLempar.setOnClickListener(this);
+        buttonUlangi.setOnClickListener(this);
+        buttonKeluar.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+
+        if (view==buttonLempar) {
+
+            Log.d("CLICK_EVENT", "Lempar button diClick!: ");
+            TextView tw=(TextView)findViewById(R.id.textView1);
+            ImageView iw=(ImageView)findViewById(R.id.imageView1);
+
+            if (acak.nextDouble() < 0.5) {
+                tw.setText("kepala");
+                iw.setImageResource(R.drawable.head);
+            } else {
+                tw.setText("Cross");
+                iw.setImageResource(R.drawable.tail);
+            }
+            buttonLempar.setVisibility(View.INVISIBLE);
+            buttonUlangi.setVisibility(View.VISIBLE);
+            buttonKeluar.setVisibility(View.VISIBLE);
+        } else if (view==buttonUlangi) {
+            TextView tw=(TextView)findViewById(R.id.textView1);
+            ImageView iw=(ImageView)findViewById((R.id.imageView1));
+
+        }else if (view==buttonKeluar){
+            this.finish();
+        }
+    }
+}
